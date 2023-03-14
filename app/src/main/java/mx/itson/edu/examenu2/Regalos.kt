81080c9e5118ba552d164756c91d145a68bf8857
Laboratorio 1821 <lv1821@itson.edu.mx>
2023-03-14 13:04:02 -0700

    first push

---
+++ app/src/main/java/mx/itson/edu/examenu2/Regalos.kt
@@ -0,0 +1,93 @@
+package mx.itson.edu.examenu2
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
+import android.widget.ImageView
+import android.widget.TextView
+
+class Regalos : AppCompatActivity() {
+
+    var regalos = ArrayList<Detalles>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regalos)
+
+        var menuOption: String? = intent.getStringExtra("menuType")
+
+        cargaRegalos(menuOption)
+        var regalosAdapter: RegaloAdapter = RegaloAdapter(this, regalos)
+        var grid: GridView = findViewById(R.id.regalos)
+        grid.adapter = regalosAdapter
+
+
+    }
+
+    fun cargaRegalos(option: String?) {
+        when(option){
+        "Peluches" -> {
+        regalos.add(Detalles(R.drawable.peluchemario, "$ 320"))
+        regalos.add(Detalles(R.drawable.pelucheminecraft, "$ 320"))
+        regalos.add(Detalles(R.drawable.peluchepeppa, "$ 290"))
+        regalos.add(Detalles(R.drawable.peluches, "$"))
+        regalos.add(Detalles(R.drawable.peluchesony, "$ 330"))
+        regalos.add(Detalles(R.drawable.peluchestich, "$ 280"))
+        regalos.add(Detalles(R.drawable.peluchevarios, "$ 195"))
+        }
+        regalos.add(Detalles(R.drawable.regaloazul, "$ 80"))
+        regalos.add(Detalles(R.drawable.regalobebe, "$ 290"))
+        regalos.add(Detalles(R.drawable.regalocajas, "$ 140"))
+        regalos.add(Detalles(R.drawable.regalocolores, "$ 85"))
+        regalos.add(Detalles(R.drawable.regalos, "$"))
+        regalos.add(Detalles(R.drawable.regalovarios, "$ 75"))
+
+        regalos.add(Detalles(R.drawable.tazaabuela, "$ 120"))
+        regalos.add(Detalles(R.drawable.tazalove, "$ 120"))
+        regalos.add(Detalles(R.drawable.tazaquiero, "$ 260"))
+        regalos.add(Detalles(R.drawable.tazas, "$ 280"))
+
+        }
+    }
+
+    class RegaloAdapter: BaseAdapter {
+        var regalos = ArrayList<Detalles>()
+        var context: Context? = null
+
+        constructor(context: Context, regalos: ArrayList<Detalles>): super() {
+            this.regalos = regalos
+            this.context = context
+        }
+
+        override fun getCount(): Int {
+            return regalos.size
+        }
+
+        override fun getItem(p0: Int): Any {
+            return regalos[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            var reg = regalos[p0]
+            var inflador = LayoutInflater.from(context)
+            var vista = inflador.inflate(R.layout.regalos, null)
+
+            var imagen = vista.findViewById(R.id.iv_regalo_imagen) as ImageView
+            var precio = vista.findViewById(R.id.tv_regalo_precio) as TextView
+
+            imagen.setImageResource(reg.image)
+            precio.setText("$${reg.precio}")
+            return vista
+        }
+    }
+}
